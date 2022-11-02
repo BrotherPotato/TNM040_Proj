b9df55c7663e2f21b71d7b42ec54485f58fe032b
PhilipRobertsson <phiro138@student.liu.se>
2022-11-02 15:04:00 +0100

    yeup

--- project/src/pages/Search.js
+++ project/src/pages/Search.js
@@ -1,8 +1,28 @@
+import { useNavigate } from 'react-router-dom'
 import searchComponent from '../components/searchComponent'
 
 function Search(){
+    const navigate = useNavigate();
+
     return(
-        <p>test test test</p>
+        <div className='parent'>
+            <div className='TopBar'>
+                <p>Sökresultat: </p>
+                <button onClick={() => navigate(-1)}>(placeholder kryss)</button>
+            </div>
+            <div className='searchResults'>
+                <table>
+                    {/*rendrera sökresultaten enligt prototyp*/}
+                </table>
+            </div>
+            <div className='SearchBar'>
+                <input
+                    type='text'
+                    label='Sök efter lokal: '
+                    placeholder='Sök efter lokal...'
+                />
+            </div>
+        </div>
     )
 }
 
