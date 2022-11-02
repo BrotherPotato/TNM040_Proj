fe24dd7c3fda6f8690a402a9d1a88551cfb72248
PhilipRobertsson <phiro138@student.liu.se>
2022-11-02 14:01:44 +0100

    LiTHe startsida

---
+++ project/src/pages/Home.js
@@ -0,0 +1,22 @@
+
+function Home(){
+ return(
+    <div className='parent'>
+        <div className='LogoTextBox'>
+            <h1>LiTHe Vilse (trademark pending)</h1> 
+        </div>
+        <div className='HouseChoice'>
+
+        </div>
+        <div className='SearchBar'>
+            <input
+                type='text'
+                label='Sök efter lokal: '
+                placeholder='Sök efter lokal...'
+            />
+        </div>
+    </div>
+ )
+}
+
+export default Home;
