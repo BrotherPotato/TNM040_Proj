c52cdbb2c6976274bfe2a9e0d6c81e88a2d3460c
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-11-06 14:27:34 +0100

    s

--- project/src/pages/SelectedRoom.js
+++ project/src/pages/SelectedRoom.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+
 
 
 const SelectedRoom = () => {
 
     return(
         <div>
+            <Link to={'/'}>
+                    <h3>Link to home page </h3>
+            </Link>
             <h1>SelectedRoom</h1>
+
         </div>
     )
 }
