b211abe500a6b034540be077e356df7accb635ad
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-11-06 14:40:11 +0100

    s

--- project/src/pages/SelectedRoom.js
+++ project/src/pages/SelectedRoom.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
+import Salar from '../components/Salar.json'
 
-
+function getRoomData(roomCode){
+    const roomData = Salar.filter(s => s.RoomCode === roomCode)[0];
+    return roomData;
+}
 
 
 const SelectedRoom = () => {
-
+    const roomCode = useParams().RoomCode;
+    const roomData = getRoomData(roomCode);
+    console.log(roomData.RoomName != '');
     return(
         <div>
-            <Link to={'/'}>
+            <Link to='/'>
                     <h3>Link to home page </h3>
             </Link>
-            <h1>SelectedRoom</h1>
+            <h1>SelectedRoom: {roomData.RoomCode}</h1>
+            {(roomData.RoomName != '') && <h2>{'RoomName: ' + roomData.RoomName}</h2>}
+
+            {(roomData.Floor != '') && <h2>{'Floor: ' + roomData.Floor}</h2>}
+
+            {(roomData.House != '') && <h2>{'House: ' + roomData.House}</h2>}
 
         </div>
     )
