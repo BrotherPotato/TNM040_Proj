1026ae42d7972a4a55a6927db68b8d79ef1134a2
PhilipRobertsson <phiro138@student.liu.se>
2022-11-06 21:25:55 +0100

    lol hus

--- project/src/pages/Home.js
+++ project/src/pages/Home.js
@@ -14,7 +14,7 @@ function Home(){
                 <Link className='HouseLink' to='/Search'>
                     <h2>Kåkenhus</h2>
                     <img className='houseImg' src={require('../images/TempHouse.jpg')} alt='Kåkenhus'/>
-                    <img src={require('../images/TempButton.jpg')} alt='Image Button'/>
+                    <img className='mapImg' src={require('../images/TempButton.jpg')} alt='Image Button'/>
                 </Link>
             </div>
              
@@ -22,7 +22,7 @@ function Home(){
                 <Link className='HouseLink' to='/Search'>
                     <h2>Täppan</h2>
                     <img className='houseImg' src={require('../images/TempHouse.jpg')} alt='Täppan'/>
-                    <img src={require('../images/TempButton.jpg')} alt='Image Button'/>
+                    <img className='mapImg' src={require('../images/TempButton.jpg')} alt='Image Button'/>
                 </Link>
             </div> 
         </div>
