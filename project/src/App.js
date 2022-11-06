842378b62b54fc54b1e3b20d3531ca38f31350c8
PhilipRobertsson <phiro138@student.liu.se>
2022-11-06 14:07:45 +0100

    css länkad korrekt i app.js

--- project/src/App.js
+++ project/src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+import './styleFolder/App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.js'
 import Search from './pages/Search.js'
