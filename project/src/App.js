9e621a8219d097330ddce8eb34e19c5235e2b7df
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-11-06 13:55:30 +0100

    added SelectedRoom.js

--- project/src/App.js
+++ project/src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.js'
 import Search from './pages/Search.js'
+import SelectedRoom from './pages/SelectedRoom.js'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path='/'>
           <Route index element={<Home />} />
           <Route path='Search' element={<Search />} />
+          <Route path='Search/:RoomCode' element={<SelectedRoom />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ project/src/pages/SelectedRoom.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const SelectedRoom = () => {
+
+    return(
+        <div>
+            <h1>SelectedRoom</h1>
+        </div>
+    )
+}
+
+export default SelectedRoom
