8ec74075555d69433235fa6203e100f52674ad9f
PhilipRobertsson <phiro138@student.liu.se>
2022-11-07 21:25:21 +0100

    lite ändringar på css i FloorSelection.js

--- project/src/pages/FloorSelection.js
+++ project/src/pages/FloorSelection.js
@@ -15,28 +15,26 @@ function FloorSelection(){
                 </Link>
             </div>
             <div className='floorSelection'>
-                <h2>Välj våning</h2>
-                <div>
-                    <div>
-                        <Link to={'/Map/'+ House + '/1'}>
-                            <button className='floorButton'>1</button>
-                        </Link>
-                        <Link to={'/Map/'+ House + '/2'}>
-                            <button className='floorButton'>2</button>
-                        </Link>
-                        <Link to={'/Map/'+ House + '/3'}>
-                            <button className='floorButton'>3</button>
-                        </Link>
-                        <Link to={'/Map/'+ House + '/4'}>
-                            <button className='floorButton'>4</button>
-                        </Link>
-                        <Link to={'/Map/'+ House + '/5'}>
-                            <button className='floorButton'>5</button>
-                        </Link>
-                    </div>
-                    <div>
-                        <img className='houseImg' src={require('../images/TempHouse.jpg')} alt='Täppan'/>
-                    </div>
+                <div className='floors'>
+                    <h2>Välj våning:</h2>
+                    <Link to={'/Map/'+ House + '/1'}>
+                        <button className='floorButton'>1</button>
+                    </Link>
+                    <Link to={'/Map/'+ House + '/2'}>
+                        <button className='floorButton'>2</button>
+                    </Link>
+                    <Link to={'/Map/'+ House + '/3'}>
+                        <button className='floorButton'>3</button>
+                    </Link>
+                    <Link to={'/Map/'+ House + '/4'}>
+                        <button className='floorButton'>4</button>
+                    </Link>
+                    <Link to={'/Map/'+ House + '/5'}>
+                        <button className='floorButton'>5</button>
+                    </Link>
+                </div>
+                <div className='houseFloor'>
+                    <img className='houseImg' src={require('../images/TempHouse.jpg')} alt='Täppan'/>
                 </div>
             </div>
         </div>
