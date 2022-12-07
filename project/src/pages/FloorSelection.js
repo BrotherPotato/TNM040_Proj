7fba321810544b281f45cfa5c017c8d75aeb8127
PhilipRobertsson <phiro138@student.liu.se>
2022-12-07 15:50:42 +0100

    lite galna commits medan jag fiskar

--- project/src/pages/FloorSelection.js
+++ project/src/pages/FloorSelection.js
@@ -17,26 +17,28 @@ function FloorSelection(){
     return(
         <div className='parent' onLoad={() => availableFloors(House)}>
             <div className='topBar'>
-                <h1 style={{color: '#FFFFFF', display: 'inline-block'}}>{House}</h1>
+                <h1 style={{color: '#FFFFFF', display: 'inline'}}>{House}</h1>
                 <img className='CloseImg' alt="Close Button" src={require('../images/TempKryss.png')} onClick={() => navigate(-1)}/>
             </div>
-            <h2 style={{color: '#FFFFFF', marginLeft: '2em'}}>Välj våning: </h2>
-            <div id='mapFloorSelection' className='whitePartFloorSelect'>
-                <Link to={'/Map/'+ House + '/5'} className='linkCSS'>
-                    <div className='floorNumber' id='floor5'>5</div>
-                </Link>
-                <Link to={'/Map/'+ House + '/4'} className='linkCSS'>
-                    <div className='floorNumber' id='floor4'>4</div>
-                </Link>
-                <Link to={'/Map/'+ House + '/3'} className='linkCSS'>
-                    <div className='floorNumber' id='floor3'>3</div>
-                </Link>
-                <Link to={'/Map/'+ House + '/2'} className='linkCSS'>
-                    <div className='floorNumber' id='floor2'>2</div>
-                </Link>
-            </div>
-            <div className='houseFloorFloorSelect'>
-                    <img src={require('../images/TempHouse.jpg')} alt='Täppan'/>
+            <h2 style={{color: '#FFFFFF', marginLeft: '2em', marginTop:'5em'}}>Välj våning: </h2>
+            <div className='Selection'>
+                <div id='mapFloorSelection' className='whitePartFloorSelect'>
+                    <Link to={'/Map/'+ House + '/5'} className='linkCSS'>
+                        <div className='floorNumber' id='floor5'>5</div>
+                    </Link>
+                    <Link to={'/Map/'+ House + '/4'} className='linkCSS'>
+                        <div className='floorNumber' id='floor4'>4</div>
+                    </Link>
+                    <Link to={'/Map/'+ House + '/3'} className='linkCSS'>
+                        <div className='floorNumber' id='floor3'>3</div>
+                    </Link>
+                    <Link to={'/Map/'+ House + '/2'} className='linkCSS'>
+                        <div className='floorNumber' id='floor2'>2</div>
+                    </Link>
+                </div>
+                <div className='houseFloorFloorSelect'>
+                        <img src={require('../images/TempHouse.jpg')} alt='Täppan'/>
+                </div>
             </div>
         </div>
     )
