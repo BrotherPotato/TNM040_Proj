55547a55502a9b236bc8350384a42fddb025240b
PhilipRobertsson <phiro138@student.liu.se>
2022-12-08 16:21:38 +0100

    nåt fel atm vetefan hur jag löser det

--- project/src/pages/SelectedRoom.js
+++ project/src/pages/SelectedRoom.js
@@ -8,13 +8,21 @@ function getRoomData(roomCode){
     return roomData;
 }
 
-
 const SelectedRoom = () => {
     const roomCode = useParams().RoomCode;
     const roomData = getRoomData(roomCode);
     //console.log(roomData.RoomName != '');    
     const navigate = useNavigate();
-
+    
+    function setMenuVisible(){
+        if(document.getElementById('selectedRoom').style.display === "none"){
+            document.getElementById('selectedRoom').style.display === "block"
+            document.getElementById('showMenu').style.display === "none"
+        }else{
+            document.getElementById('selectedRoom').style.display === "none"
+            document.getElementById('showMenu').style.display === "block"
+        }
+    }
     /*
     <Link to='/'>
         <h3>Link to home page </h3>
@@ -31,33 +39,40 @@ const SelectedRoom = () => {
             <div>
                 <img style={{width: '85%', margin: '5%', border: '5px solid #3DD2DC', borderRadius: '2px'}} className='RoomImg' alt="Room Image" src={require('../images/TempPrinter.png')}/>
             </div>
-            <div className='selectedRoom' style={{backgroundColor: '#FFFFFF'}}>
+            <div id='showMenu'style={{backgroundColor: '#FFFFFF', display: 'block'}}> 
+                <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <img style={{width: '10vw', height: '10vw', alignSelf: 'center'}} className='' alt='' src={require('../images/TempSlider.png')}/>
+                    <p>Anpassa rutt</p>
+                    <img style={{marginRight:'1em',marginTop: '0px', width: '5vw', height: '5vw', alignSelf: 'center'}} alt="Visa meny" src={require('../images/TempArrow.png')} onClick={()=>setMenuVisible()}/>
+                </div>
+            </div>
+            <div id='selectedRoom' style={{backgroundColor: '#FFFFFF', display: 'none'}}>
                 <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
                     <img style={{width: '10vw', height: '10vw', alignSelf: 'center'}} className='' alt='' src={require('../images/TempSlider.png')}/>
                     <p style={{marginLeft:"-4em"}}>Anpassa rutt</p>
-                    <img style={{marginLeft: '40vw',marginTop: '0px', width: '5vw', height: '5vw', alignSelf: 'center'}} className='' alt='' src={require('../images/TempArrow.png')}/>
+                    <img style={{marginLeft: '40vw',marginTop: '0px', width: '5vw', height: '5vw', alignSelf: 'center'}} className='' alt='' src={require('../images/TempArrow.png')} onClick={()=>setMenuVisible()}/>
                 </div>
 
                 <div style={{width: '100%', height: '1px', backgroundColor: 'black', margin: 'auto'}}></div>
 
-                    <p style={{fontSize: '1.1em', marginLeft: '11vw', fontWeight: 700}}>Nuvarande position:</p>
+                    <p style={{fontSize: '1.1em', marginLeft: '12vw', fontWeight: 700}}>Nuvarande position:</p>
                     <div style={{display: 'flex', flexDirection: 'row', marginBottom: '15px', marginTop: '10px'}}>
                         <img style={{width: '8vw', height: '8vw', alignSelf:'center', filter:'invert(1)'}} alt='Location icon' src={require('../images/TempCenterMap.png')}/>
                         <div style={{border: '1px solid black', borderRadius: '15px', width: '78vw', marginLeft: '1em'}}>
                             <p style={{margin: '5%'}}>{roomData.RoomCode}</p>
                         </div>
                     </div>
-                    <p style={{fontSize: '1.1em', marginLeft: '11vw', fontWeight: 700}}>Destination:</p>
+                    <p style={{fontSize: '1.1em', marginLeft: '12vw', fontWeight: 700}}>Destination:</p>
                     <div style={{display: 'flex', flexDirection: 'row', marginBottom: '15px',marginTop: '10px'}}>
-                        <img style={{width: '8vw', height: '8vw',alignSelf:'center'}} alt='Center map icon' src={require('../images/TempLocation.png')}/>
+                        <img style={{width: '8vw', height: 'auto',alignSelf:'center'}} alt='Center map icon' src={require('../images/TempLocation.png')}/>
                         <div style={{border: '1px solid black', borderRadius: '15px', width: '78vw', marginLeft: '1em'}}>
                             <p style={{margin: '5%'}}>{roomData.RoomCode}</p>
                         </div>
                     </div>
                     
                     <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', margin: '10px', marginTop: '20px'}}>
-                        <button style={{backgroundColor: '#3DD2DC', borderRadius: '5px', padding: '5px'}}><img style={{width: '5vw', height: '5vw'}} src={require('../images/TempLocation.png')}></img> YOOOOOOOOoo</button>
-                        <button style={{backgroundColor: '#3DD2DC', borderRadius: '5px', padding: '5px'}}><img style={{width: '5vw', height: '5vw'}} src={require('../images/TempLocation.png')}></img> YOOOOOOOOoo</button>
+                        <button className='selectedOptions'><img style={{width: '5vw', height: 'auto', marginRight: '0.5em'}} src={require('../images/TempLocation.png')}></img>Invertera valen</button>
+                        <button className='selectedOptions'><img style={{width: '5vw', height: 'auto', marginRight: '0.5em'}} src={require('../images/TempLocation.png')}></img>Visa rutt</button>
 
                     </div>
 
