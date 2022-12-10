fca88a6f30ff8c3135fac79f44cf9627e52cf547
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-12-10 17:36:51 +0100

    sorted

--- project/src/App.js
+++ project/src/App.js
@@ -1,50 +1,12 @@
 import './styles/App.css'
-import { BrowserRouter, Routes, Route, useLocation, Outlet } from 'react-router-dom'
-import Layout from "./pages/Layout.js"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AnimationLayout from "./pages/AnimationLayout.js"
 import Home from './pages/Home.js'
 import Search from './pages/Search.js'
 import SelectedRoom from './pages/SelectedRoom.js'
 import FloorSelection from './pages/FloorSelection.js'
 import Map from './pages/Map.js'
 import NavigationScreen from './pages/NavigationScreen'
-import { motion } from "framer-motion";
-
-const PageLayout = ({ children }) => children;
-
-const pageVariants = {
-  initial: {
-    opacity: 0
-  },
-  in: {
-    opacity: 1
-  },
-  out: {
-    opacity: 0
-  }
-};
-
-const pageTransition = {
-  type: "tween",
-  ease: "linear",
-  duration: 0.2
-};
-
-const AnimationLayout = () => {
-  const { pathname } = useLocation();
-  return (
-    <PageLayout>
-      <motion.div
-        key={pathname}
-        initial="initial"
-        animate="in"
-        variants={pageVariants}
-        transition={pageTransition}
-      >
-        <Outlet />
-      </motion.div>
-    </PageLayout>
-  );
-};
 
 function App() {
   return (
