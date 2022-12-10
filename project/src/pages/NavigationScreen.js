507e095307a93f3052977ff72a31fc4d846499e9
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-12-10 16:18:32 +0100

    new page

--- project/src/pages/NavigationScreen.js
+++ project/src/pages/NavigationScreen.js
@@ -3,7 +3,13 @@ import { Link, useNavigate} from 'react-router-dom'
 import { useParams } from 'react-router-dom';
 
 function NavigationScreen(){
+    const RoomCode = useParams().RoomCode;
 
+    return(<div>
+        <h1>NavigationScreen</h1>
+        <h2>{RoomCode}</h2>
+
+    </div>)
 }
 
 
