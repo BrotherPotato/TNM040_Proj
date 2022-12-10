93c7c1929cba608526568a85c7bd194b65b9207c
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-12-10 16:09:15 +0100

    new page

---
+++ project/src/pages/NavigationScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link, useNavigate} from 'react-router-dom'
+import { useParams } from 'react-router-dom';
+
+function NavigationScreen(){
+
+}
+
+
+export default NavigationScreen;
