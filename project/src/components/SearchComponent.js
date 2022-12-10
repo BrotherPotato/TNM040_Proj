3caf02652d8cc76888fec3d94ff14a681a6a3f4f
PhilipRobertsson <phiro138@student.liu.se>
2022-12-10 14:38:15 +0100

    funni arows :)

--- project/src/components/SearchComponent.js
+++ project/src/components/SearchComponent.js
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom'
 const SearchComponent = ({data}) =>{
     return(
         <div>
-            <li className='singleSearchResult'>
-                <Link className='resultLink' to={'/Search/'+ data.RoomCode}>
-                    <h3 style={{marginLeft:'10px'}}>{data.RoomCode} {data.RoomName} </h3>
-                </Link>
-
-                <p style={{marginLeft:'10px'}}>{data.House} våning: {data.Floor}</p>
+            <li className='singleSearchResult' style={{display: 'flex', justifyContent: 'space-between'}}>
+                <div>
+                    <Link className='resultLink' to={'/Search/'+ data.RoomCode}>
+                        <h3 style={{marginLeft:'10px'}}>{data.RoomCode} {data.RoomName} </h3>
+                    </Link>
+                    <p style={{marginLeft:'10px'}}>{data.House} våning {data.Floor}</p>
+                </div>
+                <img id='searchResultsArrow' style={{marginRight:'10px', width: '5vw', height: '5vw', alignSelf: 'center'}} alt="högerpil" src={require('../images/TempArrow.png')}/>
             </li>
         </div>
         
