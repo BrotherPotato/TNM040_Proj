c0fc14e35eaec32f426822d692574063069b0612
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-12-11 11:45:19 +0100

    woooo

--- project/src/pages/NavigationScreen.js
+++ project/src/pages/NavigationScreen.js
@@ -28,16 +28,6 @@ function NavigationScreen(){
         }
     }
 
-    const inputString = (e) => {
-        setCurrString(e.target.value)
-    }
-
-    function invertChoice(){
-        let curr = document.getElementById('dest').placeholder
-        document.getElementById('dest').placeholder = CurrString
-        document.getElementById('curr').value = curr
-    }
-
     return(<div>
         <div className='topBar'>
             <h1 style={{color: '#FFFFFF', display: 'inline-block'}}>{roomData.RoomCode}</h1>
@@ -56,34 +46,11 @@ function NavigationScreen(){
                 </div>
 
                 <div style={{width: '100%', height: '1px', backgroundColor: 'black', margin: 'auto'}}></div>
-
-                    <p style={{fontSize: '1.1em', marginLeft: '12vw', fontWeight: 700}}>Nuvarande position:</p>
-                    <div style={{display: 'flex', flexDirection: 'row', marginBottom: '15px', marginTop: '10px'}}>
-                        <img style={{width: '8vw', height: 'auto', alignSelf:'center', filter:'invert(1)'}} alt='Location icon' src={require('../images/TempCenterMap.png')}/>
-                        <input id='curr' type='text' placeholder='Skriv in närmaste lokal...' onInput={inputString}
-                        style={{border: '1px solid black', borderRadius: '15px', width: '78vw', marginLeft: '1em', height: '5.5vh', padding: '10px'}}>
-                        </input>
+                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                        <p style={{fontSize: '1.1em', marginLeft: '12vw', fontWeight: 700}}>Startposition: {roomData.RoomCode}</p>
+                        <p style={{fontSize: '1.1em', marginRight: '12vw', fontWeight: 700}}>Slutposition: {roomData.RoomCode}</p>
                     </div>
-                    <p style={{fontSize: '1.1em', marginLeft: '12vw', fontWeight: 700}}>Destination:</p>
-                    <div style={{display: 'flex', flexDirection: 'row', marginBottom: '15px',marginTop: '10px'}}>
-                        <img style={{width: '8vw', height: 'auto', alignSelf:'center'}} alt='Center map icon' src={require('../images/TempLocation.png')}/>
-                        <input id='dest' type='text' placeholder={roomData.RoomCode} disabled
-                        style={{border: '1px solid black', borderRadius: '15px', width: '78vw', marginLeft: '1em', height: '5.5vh', padding: '10px'}}>
-                        </input>
-                    </div>
-                    
-                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', margin: '10px', marginTop: '20px'}}>
-                        <button className='selectedOptions' onClick={()=>invertChoice()}>
-                            <img style={{width: '5vw', height: 'auto', marginRight: '0.5em'}} src={require('../images/TempLocation.png')}></img>
-                            Invertera valen
-                        </button>
-                        <Link className='resultLink' to={'/NavigationScreen/'+ RoomCode}>
-                            <button className='selectedOptions'>
-                                <img style={{width: '5vw', height: 'auto', marginRight: '0.5em'}} src={require('../images/TempLocation.png')}></img>
-                                Visa rutt
-                            </button>
-                        </Link>
-                    </div>
+                 
                 </div>
             </div>
     )
